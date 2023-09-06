dfd12b84b0d4ed4a53626a9e0b0ac75fa62c77c9
Bogdan Dancu <bogdan@domain.local>
2023-09-06 21:35:50 +0200

    adds ec2 user_data test script & aws security group

--- ch2/main.tf
+++ ch2/main.tf
@@ -3,20 +3,42 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami                         = "ami-0f409bae3775dc8e5"
+resource "aws_instance" "tf_ec2_ex1" {
+  ami                         = "ami-0261755bbcb8c4a84"
   instance_type               = "t3a.small"
   associate_public_ip_address = true
 
+  vpc_security_group_ids = [aws_security_group.sec_gr_ex1.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
-    Name = "tf-ec2-example"
+    Name = "tf-ec2-ex1"
   }
 }
 
-output "ec2_instance_availability_zone" {
-  value = aws_instance.example.availability_zone
+resource "aws_security_group" "sec_gr_ex1" {
+  name = "tf-sec_gr_ex1"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
-output "ec2_instance_subnet" {
-  value = aws_instance.example.subnet_id
+output "ec2_instance_availability_zone" {
+  value = aws_instance.tf_ec2_ex1.availability_zone
+}
+
+output "ec2_instance_public_ip" {
+  value = aws_instance.tf_ec2_ex1.public_ip
 }
